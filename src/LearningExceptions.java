f4e3317345a64556d7b4274d139ec4b5a71c0417
JJ <janarjanrb@gmail.com>
2025-06-05 23:42:31 +0545

    fifth or sixth

--- src/LearningExceptions.java
+++ src/LearningExceptions.java
@@ -1,5 +1,14 @@
 // Runtime errors are known as exceptions we may not be able to avoid errors we can mitigate or handle the occurred errors.
-// Errors are something that can't be handled
+// Errors are something that can't be handled. Exceptions are events caused by errors that causes disruption in the normal flow during the
+// runtime of the code.
+/*
+* There are mainly two types of exception i.e.:
+* 1> Runtime (Unchecked)
+*    Unchecked exceptions are not compulsory to be handled like ArrayOutOfBound, NullPointerException, ArithmeticException etc
+* 2> CompileTime (Checked)
+*    Checked exceptions are the exception which are compulsorily necessary to be handled like IOException, SQLException.
+*
+* */
 
 import java.util.Random;
 
