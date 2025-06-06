e06200c47af034344014458d5faf28316a84d717
JJ <janarjanrb@gmail.com>
2025-06-06 23:52:24 +0545

    last

---
+++ src/ImplementingRunnable.java
@@ -0,0 +1,31 @@
+class runner implements Runnable{
+    public void run(){
+    for (int i = 0; i < 100;i++){
+        System.out.println("Thread ONE is up");
+    }
+    }
+}
+
+class runner2 implements Runnable{
+    public void run(){
+    for (int i = 0;i<100;i++)
+        System.out.println("Thread TWO");
+    }
+}
+
+public class ImplementingRunnable {
+    public static void main(String[] args) {
+        Runnable obj1 = new runner(); // since runner is a class reference we can just use Runnable.
+        runner2 obj2 = new runner2();
+
+        // And we have to create Thread to create thread.
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.setPriority(8);
+        t1.start();
+        t2.start();
+
+    }
+}
