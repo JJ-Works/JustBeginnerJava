71bbc50d418f7177ed47313900a99a98d8611092
JJ <janarjanrb@gmail.com>
2025-06-08 20:41:26 +0545

    upload|

---
+++ src/SortingList.java
@@ -0,0 +1,5 @@
+public class SortingList {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/Try.java
@@ -0,0 +1,15 @@
+// var can only be used as local variable. you can use it as keyword as well as variable name but cannot be used as class name
+
+public class Try{
+public static void main(String[] a){
+    System.out.println("Hello");
+
+    // you can use var to make array but don't use square bracket for declaration.
+    var array = new int[];
+    var cat = 22;
+    var ca = 55;
+    var string = "catter";
+    System.out.println(cat);
+
+    var object = new ClassName();// This also works.
+}}
